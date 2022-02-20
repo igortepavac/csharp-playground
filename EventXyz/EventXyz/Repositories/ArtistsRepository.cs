c0382257374824f8f7b29c0096db1f57ac82534c
Igor Tepavac <itepavac@vub.hr>
2022-02-20 22:34:32 +0100

    Implement base entity repository

--- EventXyz/EventXyz/Repositories/ArtistsRepository.cs
+++ EventXyz/EventXyz/Repositories/ArtistsRepository.cs
@@ -6,13 +6,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace EventXyz.Repositories {
-    public class ArtistsRepository {
+    public class ArtistsRepository : BaseEntityRepository<Artist> {
 
-        public List<Artist> GetItems() {
-            var list = new List<Artist>();
-            list.Add(new Artist(1, "Gnork", "Pop"));
-            list.Add(new Artist(2, "Valentino", "Rock"));
-            return list;
+        private List<Artist> artists = new List<Artist>() {
+            new Artist(1, "Gnork", "Pop"),
+            new Artist(2, "Valentino", "Rock")
+        };
+
+        public override List<Artist> GetItems() {
+            return artists;
+        }
+
+        public override Artist GetItem(int id) {
+            return artists.Find(artist => artist.Id == id);
+        }
+
+        protected override void AddItemInternal(Artist artist) {
+            artists.Add(artist);
+        }
+
+        protected override void UpdateItemInternal(Artist artist) {
+            artists = artists.Select(a => a.Id == artist.Id ? artist : a).ToList();
+        }
+
+        protected override void DeleteItemInternal(int id) {
+            artists = artists.FindAll(artist => !(artist.Id == id));
         }
     }
 }
---
+++ EventXyz/EventXyz/Repositories/BaseEntityRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventXyz.Repositories {
+
+    public delegate void DataChangeObserver();
+
+    public abstract class BaseEntityRepository<T> {
+
+        public event DataChangeObserver DataChange;
+
+        public abstract List<T> GetItems();
+        public abstract T GetItem(int id);
+
+        protected abstract void AddItemInternal(T item);
+        protected abstract void UpdateItemInternal(T item);
+        protected abstract void DeleteItemInternal(int id);
+
+        public void AddItem(T item) {
+            PerformAndNotify(() => AddItemInternal(item));
+        }
+
+        public void UpdateItem(T item) {
+            PerformAndNotify(() => UpdateItemInternal(item));
+        }
+
+        public void DeleteItem(int id) {
+            PerformAndNotify(() => DeleteItemInternal(id));
+        }
+
+        private void PerformAndNotify(Action action) {
+            action.Invoke();
+            DataChange.Invoke();
+        }
+    }
+}
